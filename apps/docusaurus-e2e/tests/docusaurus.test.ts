64f20db568a7cd35d1249ce7f1d392364e0124b9
Zachary Williams <zachjw34@gmail.com>
2020-04-26 14:53:52 -0500

    feat: add e2e tests

--- apps/docusaurus-e2e/tests/docusaurus.test.ts
+++ apps/docusaurus-e2e/tests/docusaurus.test.ts
@@ -1,48 +1,20 @@
 import {
-  checkFilesExist,
   ensureNxProject,
-  readJson,
   runNxCommandAsync,
   uniq
 } from '@nrwl/nx-plugin/testing';
+
 describe('docusaurus e2e', () => {
-  it('should create docusaurus', async done => {
-    const plugin = uniq('docusaurus');
+  it('should create and build docusaurus', async done => {
+    const appName = uniq('docusaurus');
     ensureNxProject('@nx-plus/docusaurus', 'dist/libs/docusaurus');
-    await runNxCommandAsync(
-      `generate @nx-plus/docusaurus:app ${plugin}`
+    await runNxCommandAsync(`generate @nx-plus/docusaurus:app ${appName}`);
+
+    const result = await runNxCommandAsync(`run ${appName}:build-docusaurus`);
+    expect(result.stdout).toContain(
+      `Success! Generated static files in dist/docusaurus/${appName}.`
     );
 
-    const result = await runNxCommandAsync(`build ${plugin}`);
-    expect(result.stdout).toContain('Builder ran');
-
     done();
-  });
-
-  describe('--directory', () => {
-    it('should create src in the specified directory', async done => {
-      const plugin = uniq('docusaurus');
-      ensureNxProject('@nx-plus/docusaurus', 'dist/libs/docusaurus');
-      await runNxCommandAsync(
-        `generate @nx-plus/docusaurus:app ${plugin} --directory subdir`
-      );
-      expect(() =>
-        checkFilesExist(`libs/subdir/${plugin}/src/index.ts`)
-      ).not.toThrow();
-      done();
-    });
-  });
-
-  describe('--tags', () => {
-    it('should add tags to nx.json', async done => {
-      const plugin = uniq('docusaurus');
-      ensureNxProject('@nx-plus/docusaurus', 'dist/libs/docusaurus');
-      await runNxCommandAsync(
-        `generate @nx-plus/docusaurus:app ${plugin} --tags e2etag,e2ePackage`
-      );
-      const nxJson = readJson('nx.json');
-      expect(nxJson.projects[plugin].tags).toEqual(['e2etag', 'e2ePackage']);
-      done();
-    });
-  });
+  }, 100000);
 });
--- libs/docusaurus/src/schematics/app/schematic.spec.ts
+++ libs/docusaurus/src/schematics/app/schematic.spec.ts
@@ -2,7 +2,6 @@ import { Tree } from '@angular-devkit/schematics';
 import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
 import { createEmptyWorkspace } from '@nrwl/workspace/testing';
 import { join } from 'path';
-
 import { AppSchematicSchema } from './schema';
 
 describe('docusaurus schematic', () => {
@@ -16,11 +15,25 @@ describe('docusaurus schematic', () => {
 
   beforeEach(() => {
     appTree = createEmptyWorkspace(Tree.empty());
+    appTree.create('.gitignore', '');
   });
 
-  it('should run successfully', async () => {
-    await expect(
-      testRunner.runSchematicAsync('app', options, appTree).toPromise()
-    ).resolves.not.toThrowError();
+  describe('--directory', () => {
+    it('should create src in the specified directory', async () => {
+      const tree = await testRunner
+        .runSchematicAsync('app', { ...options, directory: 'subdir' }, appTree)
+        .toPromise();
+      expect(tree.exists('apps/subdir/test/docusaurus.config.js')).toBeTruthy();
+    });
+  });
+
+  describe('--tags', () => {
+    it('should add tags to nx.json', async () => {
+      const tree = await testRunner
+        .runSchematicAsync('app', { ...options, tags: 'tag1,tag2' }, appTree)
+        .toPromise();
+      const nxJson = JSON.parse(tree.readContent('nx.json'));
+      expect(nxJson.projects['test'].tags).toEqual(['tag1', 'tag2']);
+    });
   });
 });
