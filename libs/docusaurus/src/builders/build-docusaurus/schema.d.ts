cfe55161914ffe0f96c869e6e970259ab72dc611
Bucky Maler <maler.bucky@gmail.com>
2020-07-19 14:43:57 -0500

    handle required options with JSON schemas and disallow unknown options

--- libs/docusaurus/src/builders/build-docusaurus/schema.d.ts
+++ libs/docusaurus/src/builders/build-docusaurus/schema.d.ts
@@ -1,7 +1,7 @@
 import { JsonObject } from '@angular-devkit/core';
 
 export interface BuildDocusaurusBuilderSchema extends JsonObject {
-  bundleAnalyzer?: boolean;
+  bundleAnalyzer: boolean;
   outputPath: string;
-  minify?: boolean;
+  minify: boolean;
 }
