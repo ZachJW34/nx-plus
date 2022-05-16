d192c7b079edb53ee57603286cb1c458b3394e2b
Boris Kubiak <kubiakboris@gmail.com>
2022-05-16 18:55:07 +0200

    feat: add nx 13 support in dep-graph patchers

--- libs/vue/patch-nx-dep-graph.js
+++ libs/vue/patch-nx-dep-graph.js
@@ -6,11 +6,8 @@ const path = require('path');
  * @see https://github.com/nrwl/nx/issues/2960
  */
 function patchNxDepGraph() {
-  const filePath = path.join(
-    process.env.INIT_CWD || '',
-    'node_modules/@nrwl/workspace/src/core/project-graph/build-dependencies/typescript-import-locator.js'
-  );
   try {
+    const filePath = getFilePath();
     const fileContent = fs.readFileSync(filePath).toString('utf-8');
     const replacement = "extension !== '.ts' && extension !== '.vue'";
     if (fileContent.includes(replacement)) {
@@ -24,6 +21,22 @@ function patchNxDepGraph() {
   } catch (err) {
     console.error('Failed to patch Nx dep-graph for Vue support.', err);
   }
+}
+
+function getFilePath() {
+  const possiblePaths = [
+    'node_modules/nx/src/project-graph/build-dependencies/typescript-import-locator.js', // for Nx >= 13.10.3
+    'node_modules/@nrwl/workspace/src/core/project-graph/build-dependencies/typescript-import-locator.js', // for older versions of Nx
+  ];
+
+  for (const p of possiblePaths) {
+    const fullPath = path.join(process.env.INIT_CWD || '', p);
+    if (fs.existsSync(fullPath)) {
+      return fullPath;
+    }
+  }
+
+  throw new Error("Could not find Nx's dep-graph builder in node_modules");
 }
 
 patchNxDepGraph();
