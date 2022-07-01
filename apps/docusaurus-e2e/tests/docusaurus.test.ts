b0cef8f008e14dafd46bdc765e68307528bffa0c
Zachary Williams <zachjw34@gmail.com>
2022-06-30 21:39:18 -0500

    I done goofed

--- apps/docusaurus-e2e/tests/docusaurus.test.ts
+++ apps/docusaurus-e2e/tests/docusaurus.test.ts
@@ -5,11 +5,11 @@ describe('docusaurus e2e', () => {
   it('should create and build docusaurus', async () => {
     const appName = uniq('docusaurus');
     ensureNxProject('@nx-plus/docusaurus', 'dist/libs/docusaurus');
-    await runNxCommandAsyncStrippedStripped(
+    await runNxCommandAsyncStripped(
       `generate @nx-plus/docusaurus:app ${appName}`
     );
 
-    const result = await runNxCommandAsyncStrippedStripped(`build ${appName}`);
+    const result = await runNxCommandAsyncStripped(`build ${appName}`);
     expect(result.stdout).toContain(
       `Success! Generated static files in "dist/apps/${appName}".`
     );
